e25b496a9b9d1023ffc78f8f33312e6bda948a8c
sriram0811 <sriram2018p@gmail.com>
2023-08-31 12:43:16 +0530

    image commit

--- src/pages/Solution.jsx
+++ src/pages/Solution.jsx
@@ -7,6 +7,9 @@ import Row from 'react-bootstrap/Row';
 import solution1 from '../Assests/solution1.png'
 import solution2 from '../Assests/solution2.png'
 import solution3 from '../Assests/solution3.png'
+import solution4 from '../Assests/solution4.png'
+import solution5 from '../Assests/solution5.png'
+import solution6 from '../Assests/solution6.png'
 
 const Solution = () => {
   return (
@@ -18,21 +21,34 @@ const Solution = () => {
       <h2 className='py-3'>Solutions</h2>
       <Container>
       <Row>
-        <Col xs={6} md={4}>
-          <Image src={solution1} alt="image"/>
+        <Col xs={6} md={4} className='py-2'>
+          <Image src={solution1} alt="image" className='rounded'/>
         </Col>
-        <Col xs={6} md={4}>
-          <Image src={solution2} alt="image"/>
+        <Col xs={6} md={4} className='py-2'>
+          <Image src={solution2} alt="image" className='rounded'/>
         </Col>
-        <Col xs={6} md={4}>
-          <Image src={solution3} alt="image"/>
+        <Col xs={6} md={4} className='py-2'>
+          <Image src={solution3} alt="image" className='rounded'/>
         </Col>
+        <Row>
+        <Col xs={6} md={4} className='py-2'>
+          <Image src={solution4} alt="image" className='rounded'/>
+        </Col>
+        <Col xs={6} md={4}className='py-2'>
+          <Image src={solution5} alt="image" className='rounded'/>
+        </Col>
+        <Col xs={6} md={4}className='py-2'>
+          <Image src={solution6} alt="image" className='rounded'/>
+        </Col>
+       
+
+        </Row>
        
        
         </Row>
       
     </Container>
-   <center> <Button variant="dark" size="lg" active>
+   <center className='py-2'> <Button variant="dark" size="lg" active>
         View all Solutions
       </Button></center>
     
